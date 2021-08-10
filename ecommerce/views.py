7c637bb35f46d174f89b89a6098570517a3cb5f8
Mrutyunjaya Behera <mrutyunjaya.9029@gmail.com>
2021-08-10 19:20:04 +0530

    updated

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,17 +5,17 @@ from django.core.paginator import Paginator
 
 def index(request):
   products = Products.objects.all()
-  
+
   # adding search functionality by geeting the user input value through 'name' attriute
   item_name = request.GET.get('item_name')
   if (item_name != " " and item_name is not None):
-    products = Products.objects.filter(title__icontains = item_name)
-    
+    products = Products.objects.filter(title__icontains=item_name)
+
   # pagination code
   paginator = Paginator(products, 4)
   page = request.GET.get('page')
   products = paginator.get_page(page)
-  
+
   # contexts used to display objects on screen
   context = {'products': products}
   return render(request, 'ecommerce/index.html', context)
